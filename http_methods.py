64be7add9d640fca0efdbbbdb31028ee2426939e
Kiartan <Krzybroda@wp.pl>
2023-04-08 17:31:47 +0200

    improvement on 2nd and start for 3rd task

--- http_methods.py
+++ http_methods.py
@@ -1,7 +1,21 @@
-from flask import Flask
-from flask import request
+from flask import Flask, redirect, request, url_for
+
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+content = None
+
+@app.get("/")
+def index_get():
+    global content
+    if content is not None:
+        return content
+    return "<p>Kontent był nullem (None)</p>"
+
+@app.post("/")
+def index_post():
+    global content
+    content = request.data.decode("utf-8")
+    return redirect(url_for('index_get'))
+    
