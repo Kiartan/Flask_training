62c5954094524b98e3246d597344b89d60426820
Kiartan <Krzybroda@wp.pl>
2023-05-09 15:35:52 +0200

    working task 3

--- persistency_test.py
+++ persistency_test.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, request, url_for
+import os.path
 
 app = Flask(__name__)
 
@@ -7,12 +8,16 @@ content = None
 @app.get('/')
 def index_get():
     global content
-    with open('test.txt', 'r') as f:
-        saved_content = f.read()
-        f.close()
-    if saved_content is not None:
-        return saved_content
-    return "<p>Ni ma contynta (None)</p>"
+    if os.path.isfile('./test.txt') == True:
+        with open('test.txt', 'r') as f:
+            saved_content = f.read()
+            f.close()
+        if saved_content is not None:
+            return saved_content
+        else:
+            return "<p>Ni ma contynta (None) w test.txt</p>"
+    else:
+        return "<p>Ni ma test.txt - nic nie wprowadzono na serwer</p>"
 
 
 @app.post('/')
