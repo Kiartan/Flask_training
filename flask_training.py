79f9a394c8a347d26fe3d0e69d445c3c8d1bb0f3
Kiartan <Krzybroda@wp.pl>
2023-05-12 13:04:34 +0200

    Update task for 3rd task. The file persistency_test.py was renamed to flask_training.py in order to keep further tasks results in one file

--- flask_training.py
+++ flask_training.py
@@ -8,23 +8,23 @@ content = None
 @app.get('/')
 def index_get():
     global content
-    if os.path.isfile('./test.txt') == True:
+    if content is not None:
+        return content
+    elif os.path.exists('./test.txt'):
         with open('test.txt', 'r') as f:
             saved_content = f.read()
-            f.close()
         if saved_content is not None:
             return saved_content
         else:
             return "<p>Ni ma contynta (None) w test.txt</p>"
     else:
-        return "<p>Ni ma test.txt - nic nie wprowadzono na serwer</p>"
+        return "<p>Ni ma test.txt, nic nie wprowadzono na serwer</p>"
 
 
 @app.post('/')
 def savePost():
     global content
     content = request.data.decode("utf-8")
-    with open('test.txt', 'a') as f:
+    with open('test.txt', 'w') as f:
         f.write(content)
-        f.close()
     return redirect(url_for('index_get'))
