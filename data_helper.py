d866dd2b7d4c35d7c887ccea17f019e3620c024a
Kiartan <Krzybroda@wp.pl>
2023-05-18 15:37:33 +0200

    Posting now returns the id given to posted data

--- data_helper.py
+++ data_helper.py
@@ -3,7 +3,16 @@ import json
 class DataHelper:
     def __init__(self):
         pass
-    
+
+    def last_id_finder(self):
+        with open('test.json', 'r') as f:
+            data = json.load(f)
+            if type(data) is dict:
+                data = [data]
+            id_list = [d.get('id', None) for d in data]
+            last_id = int(id_list[-1])
+            return(last_id)
+        
     def id_generator(self, last_id):
         i = self.last_id_finder()
         return(i+1)
@@ -15,12 +24,12 @@ class DataHelper:
     def json_updater(self, last_id, content):
         new_id = self.id_generator(last_id)
         new_post = self.posted_content_writer(new_id, content)
-        with open('data.json', 'r') as f:
+        with open('test.json', 'r') as f:
             data = json.load(f)
             if type(data) is dict:
                 data = [data]
             data.append(new_post)
-        with open('data.json', 'w') as j:
+        with open('test.json', 'w') as j:
             json.dump(data, j, indent = 4)
         return(new_id)
     
@@ -28,17 +37,10 @@ class DataHelper:
         with open('./test.txt', 'r') as f:            
             txt_content = f.read()
         if txt_content is not None:
-            with open('./data.json','w') as j:
+            with open('./test.json','w') as j:
                 saved_content = self.posted_content_writer(0, txt_content)
                 json.dump(saved_content, j, indent = 4)
         return (0)
 
-    def last_id_finder(self):
-        with open('data.json', 'r') as f:
-            data = json.load(f)
-            if type(data) is dict:
-                data = [data]
-            id_list = [d.get('id', None) for d in data]
-            last_id = int(id_list[-1])
-            return(last_id)
+
                 
--- flask_training.py
+++ flask_training.py
@@ -15,14 +15,14 @@ def index_get():
     global content
     requested_id = request.data.decode("utf-8")
     req_id = int(requested_id)
-    if os.path.exists('./data.json'):
-        with open('data.json', 'r') as f:
+    if os.path.exists('./test.json'):
+        with open('test.json', 'r') as f:
            stored_data = json.load(f)
         requested_data = next(iter(item for item in stored_data if item['id'] == req_id), None)
         if requested_data is not None:
             return requested_data
         else:
-            return ("<p>Ni ma wpisu o takim id byczq, sprawdź czy się nie machnąłeś</p>")
+            return "<p>Ni ma wpisu o takim id byczq, sprawdź czy się nie machnąłeś</p>"
 
 
 
@@ -30,12 +30,9 @@ def index_get():
 def save_post():
     global content
     content = request.data.decode("utf-8")
-    if os.path.exists('./data.json') == False:
+    if os.path.exists('./test.json') == False:
         last_id = data_helper.txt_to_json_transformator()
     else: 
         last_id = data_helper.last_id_finder()
     id_new_content = data_helper.json_updater(last_id, content)
-    print(id_new_content)
-    #return "<p>ID twojego posta to</p>" (id_new_content, redirect(url_for('index_get')))
-    #return str(id_new_content)
-    return redirect(url_for('index_get'))
+    return "<p>ID twojego wpisu to: </p>" + str(id_new_content)
