455f203040fc42b411a106b18589e8c91e692cd5
Kiartan <Krzybroda@wp.pl>
2023-05-23 12:33:46 +0200

    Some fixes after feedback

--- data_helper.py
+++ data_helper.py
@@ -1,54 +1,51 @@
 import json
 
-class DataHelper:
-    def __init__(self):
-        pass
 
-    def last_id_finder(self):
-        with open('test.json', 'r') as f:
-            data = json.load(f)
-            if type(data) is dict:
-                data = [data]
-            id_list = [d.get('id', None) for d in data]
-            last_id = int(id_list[-1])
-            return(last_id)
-        
-    def id_generator(self, last_id):
-        i = self.last_id_finder()
-        return(i+1)
+def find_last_id():
+    with open('test.json', 'r') as f:
+        data = json.load(f)
+        if type(data) is dict:
+            data = [data]
+        id_list = [d.get('id', None) for d in data]
+        last_id = int(id_list[-1])
+        return(last_id)
 
-    def posted_content_writer(self, n, content):
-        updated_content={'id': n, 'posted_data': content}
-        return(updated_content)
+def write_posted_content(n, content):
+    #updated_content={'id': n, 'posted_data': content}
+    updated_content={n: {'posted_data': content}}
+    return(updated_content)
 
-    def json_updater(self, last_id, content):
-        new_id = self.id_generator(last_id)
-        new_post = self.posted_content_writer(new_id, content)
-        with open('test.json', 'r') as f:
-            data = json.load(f)
-            if type(data) is dict:
-                data = [data]
-            data.append(new_post)
-        with open('test.json', 'w') as j:
-            json.dump(data, j, indent = 4)
-        return(new_id)
+def update_json(last_id, content):
+    new_id = last_id + 1
+    new_post = write_posted_content(new_id, content)
+    with open('test.json', 'r') as f:
+        data = json.load(f)
+        if type(data) is dict:
+            data = [data]
+        data.append(new_post)
+    with open('test.json', 'w') as j:
+        json.dump(data, j, indent = 3)
+    return(new_id)
     
-    def txt_to_json_transformator(self):
-        with open('./test.txt', 'r') as f:            
-            txt_content = f.read()
-        if txt_content is not None:
-            with open('./test.json','w') as j:
-                saved_content = self.posted_content_writer(0, txt_content)
-                json.dump(saved_content, j, indent = 4)
-        init_id = int(0)
-        return(init_id)
+def transform_txt_to_json():
+    with open('./test.txt', 'r') as f:            
+        txt_content = f.read()
+    if txt_content is not None:
+        with open('./test.json','w') as j:
+            saved_content = write_posted_content(0, txt_content)
+            json.dump(saved_content, j, indent = 3)
+    init_id = int(0)
+    return(init_id)
     
-    def requested_data_finder(self, req_id):
-        searched_id = int(req_id)
-        with open('test.json', 'r') as f:
-           stored_data = json.load(f)
-        requested_data = next(iter(item for item in stored_data if item['id'] == searched_id), None)
-        return(requested_data)
+def find_requested_data(req_id):
+    #searched_id = int(req_id)
+    with open('test.json', 'r') as f:
+        stored_data = json.load(f)
+    #requested_data = next(iter(item for item in stored_data if item['id'] == searched_id), None)
+    for i in stored_data:
+        if i == req_id:
+            return(i['posted_data'])
+    #return(requested_data)
 
 
                 
--- flask_training.py
+++ flask_training.py
@@ -1,12 +1,11 @@
 from flask import Flask, redirect, request, url_for
 import os.path
 import json
-from data_helper import DataHelper
+from data_helper import *
 
 
 app = Flask(__name__)
 
-data_helper = DataHelper()
 
 content = None
 
@@ -17,13 +16,13 @@ def index_get():
     if requested_id.isnumeric():
         req_id = int(requested_id)
         if os.path.exists('./test.json'):
-            requested_data = data_helper.requested_data_finder(req_id)
+            requested_data = find_requested_data(req_id)
             if requested_data is not None:
                 return requested_data
             else:
                 return "<p>Ni ma wpisu o takim id byczq, sprawdź czy się nie machnąłeś</p>"
         else:
-            data_helper.txt_to_json_transformator()
+            transform_txt_to_json()
             with open('test.json', 'r') as f:
                 requested_data = json.load(f)
             return "<p>Jedyny wpis w bazie to: </p>" + str(requested_data)
@@ -38,8 +37,8 @@ def save_post():
     global content
     content = request.data.decode("utf-8")
     if os.path.exists('./test.json'):
-        last_id = data_helper.last_id_finder()
+        last_id = find_last_id()
     else: 
-        last_id = data_helper.txt_to_json_transformator()
-    id_new_content = data_helper.json_updater(last_id, content)
+        last_id = transform_txt_to_json()
+    id_new_content = update_json(last_id, content)
     return "<p>ID twojego wpisu to: </p>" + str(id_new_content)
